fd31caca7b3a2c64790d8ea7a6817dc5f2235d36
Ninja-21-dev <ninja.cooldev@gmail.com>
2021-09-02 21:49:09 +0430

    created a custom User model

--- users/models.py
+++ users/models.py
@@ -1,13 +1,38 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractBaseUser
 
+from users.managers import MyUserManager
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=100, unique=True)
+    full_name = models.CharField(max_length=100)
+    
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    
+    objects = MyUserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+    
 
 class JobSeeker(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	is_jobseeker = models.BooleanField(default=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_jobseeker = models.BooleanField(default=True)
 
-	def __str__(self):
-    		return f'{self.user}'
-
-	def save(self, *args, **kwargs):
-		super(JobSeeker, self).save(*args, **kwargs)
+    def __str__(self):
+        return str(self.user)
