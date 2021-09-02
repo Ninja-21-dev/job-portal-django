8abe9cd61cc2c9c57ac3abba065c681a157e2752
Ninja-21-dev <ninja.cooldev@gmail.com>
2021-09-02 21:47:56 +0430

    created employer views

--- portal/views.py
+++ portal/views.py
@@ -1,7 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
+from django.contrib.auth.decorators import login_required
 
 from users.models import JobSeeker
+from .forms import CompanyCreationForm
+from .models import Company
 
 
-def home_page(request):
-    return render(request, 'home_page.html')
+@login_required
+def employer_home(request):
+    """ show list of requests by jobseekers on jobs created by employer"""     
+    if request.user.jobseeker.is_jobseeker:     
+            raise Http404
+    return render(request, 'employer/home.html')
+
+
+@login_required
+def employer_profile(request):
+    """ 
+    create company profile by getting information from employer by
+    CompanyCreationForm from Company model.
+    """
+    if request.user.jobseeker.is_jobseeker:     
+            raise Http404
+    if request.method == 'POST':
+        form = CompanyCreationForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.employer = request.user
+            instance.save()
+    form = CompanyCreationForm()
+    return render(request, 'employer/profile.html', {'form':form})
+
+
+@login_required
+def employer_jobs(request):
+    """ show all jobs published by the employer/company."""
+    if request.user.jobseeker.is_jobseeker:     
+            raise Http404
+    return render(request, 'employer/jobs.html')
+
+
+@login_required
+def employer_create_job(request):
+    """ create a job and publishe it to the portal"""
+    if request.user.jobseeker.is_jobseeker:     
+            raise Http404
+    return render(request, 'employer/create_job.html')
