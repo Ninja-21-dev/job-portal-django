8ac7881f85b097127d906de2f1876b3a818efa0f
Ninja-21-dev <ninja.cooldev@gmail.com>
2021-09-06 22:06:19 +0430

    added message tags variable

--- OnlineJobPortalProject/settings.py
+++ OnlineJobPortalProject/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +43,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
 
 ROOT_URLCONF = 'OnlineJobPortalProject.urls'
 
