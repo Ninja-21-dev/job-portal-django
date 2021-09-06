a1b5b8d269df94f93aba768004d2cef0a7c86153
Ninja-21-dev <ninja.cooldev@gmail.com>
2021-09-06 22:30:23 +0430

    added cancel request view path

--- jobseeker/urls.py
+++ jobseeker/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
-from .views import jobseeker_profile, request_job, jobseeker_requests, save_job, jobseeker_saved_jobs
+from .views import (
+    jobseeker_profile,
+    request_job,
+    jobseeker_requests, 
+    save_job,
+    jobseeker_saved_jobs,
+    cancel_request,
+)
 
 urlpatterns = [
     path('profile/', jobseeker_profile, name='jobseeker-profile'),
@@ -8,4 +15,5 @@ urlpatterns = [
     path('requests/', jobseeker_requests, name='jobseeker-requests'),
     path('save/job/<int:id>/', save_job, name='save-job'),
     path('saved/', jobseeker_saved_jobs, name='saved-jobs'),
+    path('cancel/request/<int:id>/',cancel_request, name='cancel-request'),
 ]
