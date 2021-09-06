d65306a6a66fb70bff6e7ff27d4338521e1b0df1
Ninja-21-dev <ninja.cooldev@gmail.com>
2021-09-06 22:08:29 +0430

    added new views to urk

--- employer/urls.py
+++ employer/urls.py
@@ -5,6 +5,9 @@ from .views import (employer_home,
                    employer_profile,
                    employer_jobs,
                    employer_create_job,
+                   accepted_status,
+                   hired_status,
+                   delete_request,
                    )
 
 
@@ -13,5 +16,9 @@ urlpatterns = [
     path('profile/', employer_profile , name='employer-profile'),
     path('jobs/', employer_jobs , name='employer-jobs'),
     path('create/job/', employer_create_job , name='employer-create-job'),
+    path('accept/request/<int:id>/', accepted_status, name='accept-status'),
+    path('hire/request/<int:id>/', hired_status, name='hire-status'),
+    path('delete/request/<int:id>/', delete_request, name='delete-request'),
     path('logout/',LogoutView.as_view(), name='logout'),
+    
 ]
